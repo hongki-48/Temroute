f214017b241d87fca0c2ed043c2eabf802856e5c
hongki-48 <kissl3oyhong@nate.com>
2025-08-29 21:32:46 +0900

    009추가, 파일분리

---
+++ js/products.js
@@ -0,0 +1,66 @@
+// Product list (외부 주입)
+window.PRODUCTS = [
+  {
+    id: "p1",
+    title: "[001]코비 DIY 백색소음기",
+    category: "디지털/가전",
+    image: "images/product/001.jpg",
+    url: "https://link.coupang.com/a/cL3l9T"
+  },
+  {
+    id: "p2",
+    title: "[002]카우 전자동 과일 야채 껍질 제거기 업소용 배출형 공식스토어 정품",
+    category: "주방",
+    image: "images/product/002.jpg",
+    url: "https://link.coupang.com/a/cMgOJg"
+  },
+  {
+    id: "p3",
+    title: "[003]누비아 1+1 욕실 세면대 스테인레스 팝업 거름망 하수구 배수구",
+    category: "욕실",
+    image: "images/product/003.jpg",
+    url: "https://link.coupang.com/a/cMoqKg"
+  },
+  {
+    id: "p4",
+    title: "[004]모락 떡 제조기",
+    category: "주방",
+    image: "images/product/004.jpg",
+    url: "https://link.coupang.com/a/cMzCIb"
+  },
+  {
+    id: "p5",
+    title: "[005]숙성 광어회",
+    category: "수산물",
+    image: "images/product/005.jpg",
+    url: "https://link.coupang.com/a/cMQOBN"
+  },
+  {
+    id: "p6",
+    title: "[006]아즈라 트리뷰트 고품질 이어팁 DAC탑재 C타입 유선 이어폰",
+    category: "디지털/가전",
+    image: "images/product/006.jpg",
+    url: "https://link.coupang.com/a/cMQQOt"
+  },
+  {
+    id: "p7",
+    title: "[007]샘스캐빈 천연 거품입욕제 아로마 버블바스",
+    category: "욕실",
+    image: "images/product/007.jpg",
+    url: "https://link.coupang.com/a/cNgVzG"
+  },
+  {
+    id: "p8",
+    title: "[008]영업용 시로코팬 바람 환풍기 모터 고기집 닥트 식당 덕트 후드 팬 환기 욕실",
+    category: "욕실",
+    image: "images/product/008.jpg",
+    url: "https://link.coupang.com/a/cNvZO6"
+  },
+  {
+    id: "p9",
+    title: "[009]그레이트가든 쌀 소금빵 완제품 개별포장 무방부제 100%천연버터 사용",
+    category: "식품",
+    image: "images/product/009.jpg",
+    url: "https://link.coupang.com/a/cNv1pm"
+  }
+];
